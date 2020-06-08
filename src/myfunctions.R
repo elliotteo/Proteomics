313c344fa96fd4822a6a1c15e48e86a2c0c31cce
Elliot <elliotjteo@gmail.com>
2020-06-08 11:42:45 +1000

    This commit produces a complete pathway analysis for the SWATH data set.

--- src/myfunctions.R
+++ src/myfunctions.R
@@ -1,7 +1,46 @@
-#myfunctions
+##### Extracting a String Between 2 Characters in R
+##### By Eric Cai - The Chemical Statistician
 
-test_function <- function(){
-
-  print("This function works")
+# getstr() is my customized function
+# it extracts a string between 2 characters in a string variable
+getstr = function(mystring, initial.character, final.character)
+{
   
+  # check that all 3 inputs are character variables
+  if (!is.character(mystring))
+  {
+    stop('The parent string must be a character variable.')
+  }
+  
+  if (!is.character(initial.character))
+  {
+    stop('The initial character must be a character variable.')
+  }
+  
+  
+  if (!is.character(final.character))
+  {
+    stop('The final character must be a character variable.')
+  }
+  
+  
+  
+  # pre-allocate a vector to store the extracted strings
+  snippet = rep(0, length(mystring))
+  
+  
+  
+  for (i in 1:length(mystring))
+  {
+    # extract the initial position
+    initial.position = gregexpr(initial.character, mystring[i])[[1]][1] + 1
+    
+    # extract the final position
+    final.position = gregexpr(final.character, mystring[i])[[1]][1] - 1
+    
+    # extract the substring between the initial and final positions, inclusively
+    snippet[i] = substr(mystring[i], initial.position, final.position)
+  }
+  
+  return(snippet)
 }
